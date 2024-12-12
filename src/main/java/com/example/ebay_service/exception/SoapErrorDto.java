3823c4ce2ea9ddc5dd055086d19997419496127b
i.elmeev <i.elmeev@vkteam.ru>
2024-12-12 22:50:31 +0300

    fix imports

--- src/main/java/com/example/ebay_service/exception/SoapErrorDto.java
+++ src/main/java/com/example/ebay_service/exception/SoapErrorDto.java
@@ -1,8 +1,8 @@
 package com.example.ebay_service.exception;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "SoapErrorDto", propOrder = {
